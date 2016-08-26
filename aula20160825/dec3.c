72d2a8f40c29d00a82bf5d1e6b28ab487e58d8ca
Ariana <arianacabral57@gmail.com>
2016-08-26 13:56:24 -0300

    fim de aula

--- aula20160825/dec3.c
+++ aula20160825/dec3.c
@@ -1,1 +1,29 @@
+#include<stdio.h>
+#include<time.h>
+int main()
+{
+    srand(time(0));
+    int dado1, dado2, dado3,soma;
 
+    dado1=("%d\n",rand()%6+1);
+    printf("\n O numero sorteado do primeiro dado:%d\n",dado1);
+    dado2=("%d\n",rand()%6+1);
+    printf("\n O numero sorteado do segundo dado:%d\n",dado2);
+    dado3=("%d\n",rand()%6+1);
+
+    printf("\n O numero sorteado do terceiro dado:%d\n",dado3);
+
+    soma=(dado1+dado2+dado3);
+    printf("\n Numero da soma:%d\n",soma);
+    if(soma==7 || soma==11)
+        {
+            printf("\n Venceu! \n");
+        }
+
+    else
+        {
+            printf("\n Perdeu! \n");
+        }
+
+    return 0;
+    }
