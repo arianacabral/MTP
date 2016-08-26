6d1b56484f0bda8e3d7bd3cbcbf743b259d6858c
Ariana <arianacabral57@gmail.com>
2016-08-26 13:53:58 -0300

    fim de aula

--- aula20160825/ger2.c
+++ aula20160825/ger2.c
@@ -2,15 +2,34 @@
 #include<time.h>
 int main()
 {
-    srand(time(0));
-    int n,cont;
-    int s = rand()%100;
+    srand (time (0));
+    int n,v;
+
+    v = ("%d", rand()%100);
+    printf("\n Numero sorteiado %d\n",v);
+
     do
     {
-        printf("\n Informe um numero entre 0 e 99 :");
+        printf("\n Informe um numero de 0 a 99 \n");
         scanf("%d",&n);
 
-
-    }while(n! = s)
+        if(0<=n<100)
+            {
+                if (n<v)
+                {
+                    printf("\n BAIXO! \n");
+                }
+                else
+                    if (n>v)
+                    {
+                        printf("\n ALTO! \n");
+                    }
+                    else
+                        {
+                            printf("\n ACERTOU! \n");
+                            break;
+                        }
+            }
+    } while (n!=v);
     return 0;
 }
