6ff788ffcd16c918fd81c03a325efe0ddbae7818
arianacabral <arianacabral57@gmail.com>
2016-09-15 12:28:54 -0300

    fim de aula

--- aula20160915/enu2.c
+++ aula20160915/enu2.c
@@ -1,49 +1,50 @@
 #include<stdio.h>
 #include<stdlib.h>
-enum Meses { janeiro = 1, fevereiro = 2, marco =3, abril =4, maio = 5, junho = 6, julho = 7, agosto = 8, setembro = 9, outubro = 10, novembro = 11, dezembro = 12};
+typedef
+enum Meses { janeiro = 1, fevereiro = 2, marco =3, abril =4, maio = 5, junho = 6, julho = 7, agosto = 8, setembro = 9, outubro = 10, novembro = 11, dezembro = 12}calen;
 
 int main ()
 {
-   enum Meses xxx;
+   calen xxx;
    printf ("\nInforme o numero do mes que voce nasceu : ");
-   scanf("%d",xxx);
+   scanf("%d",&xxx);
    switch (xxx)
 		{
 		case janeiro:
-			printf("\nVoce nasceu em JANEIRO\n");
+			printf("\nVoce nasceu em janeiro\n");
 			break;
         case fevereiro:
-			printf("\nVoce nasceu em FEVEREIRO\n");
+			printf("\nVoce nasceu em fevereiro\n");
 			break;
         case marco:
-			printf("\nVoce nasceu em MARCO\n");
+			printf("\nVoce nasceu em marco\n");
 			break;
         case abril:
-			printf("\nVoce nasceu em ABRIL\n");
+			printf("\nVoce nasceu em abril\n");
 			break;
         case maio:
-			printf("\nVoce nasceu em MAIO\n");
+			printf("\nVoce nasceu em maio\n");
 			break;
         case junho:
-			printf("\nVoce nasceu em JUNHO\n");
+			printf("\nVoce nasceu em junho\n");
 			break;
         case julho:
-			printf("\nVoce nasceu em JULHO\n");
+			printf("\nVoce nasceu em julho\n");
 			break;
         case agosto:
-			printf("\nVoce nasceu em AGOSTO\n");
+			printf("\nVoce nasceu em agosto\n");
 			break;
         case setembro:
-			printf("\nVoce nasceu em SETEMBRO\n");
+			printf("\nVoce nasceu em setembro\n");
 			break;
         case outubro:
-			printf("\nVoce nasceu em OUTUBRO\n");
+			printf("\nVoce nasceu em outubro\n");
 			break;
         case novembro:
-			printf("\nVoce nasceu em NOVEMBRO\n");
+			printf("\nVoce nasceu em novembro\n");
 			break;
         case dezembro:
-			printf("\nVoce nasceu em DEZEMBRO\n");
+			printf("\nVoce nasceu em dezembro\n");
 			break;
 		}
 
