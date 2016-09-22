5f65e5e6bec7ec2c1f118a799b41f89ff7db5b67
arianacabral <arianacabral57@gmail.com>
2016-09-22 12:40:04 -0300

    fim de aula

--- aula20160922/sacr2.c
+++ aula20160922/sacr2.c
@@ -1,3 +1,37 @@
-#include<stdio.h>
+#include <stdio.h>
 #include<stdlib.h>
-#include<time.h>
+#include <time.h>
+
+int sorteia_dados();
+
+int main()
+{
+srand(time(0));
+int i , soma=0, tentativa = 0;
+do{
+soma = 0;
+for (i = 0; i < 5; i++)
+{
+sorteia_dados();
+soma = soma + sorteia_dados();
+}
+if(soma >= 21)
+{
+printf("\n Voce ganhou na %d tentativa!!!\n", tentativa+1);
+break;
+}
+else
+{
+printf("\n Voce perdeu na %d tentativa!!!\n", tentativa+1 );
+}
+tentativa++;
+
+} while(tentativa < 3);
+return 0;
+}
+
+int sorteia_dados()
+{
+int dado = rand()%6 + 1;
+return dado;
+}
